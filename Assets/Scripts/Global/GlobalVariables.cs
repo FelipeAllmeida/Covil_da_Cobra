daae71b980d464b8769bc2276bf87a8eb44a5c35
ODIN\Majerkowski <majerkowski@live.com>
2016-11-08 23:05:51 -0200

    jogo em andamento seus cobras

---
+++ Assets/Scripts/Global/GlobalVariables.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class GlobalVariables
+{
+    public static string personagemSelecionado;
+    public static GameObject[] ListaPersonagens;
+}
---
+++ Assets/Scripts/MapEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[CustomEditor(typeof(MapScript))]
+public class MapEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        MapScript map = target as MapScript;
+
+        // map.GenerateMap();
+    }
+
+}
